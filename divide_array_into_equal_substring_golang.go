a4b4b6e48a72a51de63c807e15174f97ba0c02a2
Manikanta <pise.manikanta@gmail.com>
2022-12-14 19:09:54 +0530

    Update divide_array_into_equal_substring_golang.go

--- divide_array_into_equal_substring_golang.go
+++ divide_array_into_equal_substring_golang.go
@@ -1,4 +1,5 @@
-
+// You can edit this code!
+// Click here and start typing.
 package main
 
 import "fmt"
@@ -7,16 +8,20 @@ func main() {
 	var ts, ss, eq int
 	//ar := []int{1, 5, 11, 5}
 	//ar := []int{1, 3, 3, 3, 3, 1}
-	ar := []int{4, 5, 1, 1, 1}
+	//ar := []int{4, 5, 1, 1, 1}
+	//ar := []int{3, 34, 4, 12, 5, 2}
+	ar := []int{34, 3, 4, 12, 5, 2, 8}
 	var s1, s2 = []int{}, []int{}
 	for i := 0; i < len(ar); i++ {
 		ts += ar[i]
 	}
+	fmt.Println("total sum", ts)
 	eq = ts / 2
 	if ts%2 != 0 {
 		fmt.Println("Cannot divide equally")
 		return
 	}
+	candivide := false
 	for i := 0; i < len(ar); i++ {
 		ss += ar[i]
 
@@ -26,6 +31,7 @@ func main() {
 
 		if ss == eq {
 			s1 = append(s1, ar[i])
+			candivide = true
 		}
 
 		if ss > eq {
@@ -33,6 +39,10 @@ func main() {
 			s2 = append(s2, ar[i])
 		}
 	}
-	fmt.Println(s1, s2)
-
+	
+	if !candivide {
+		fmt.Println("Unable to divide in equally subarray")
+		return
+	}
+	fmt.Println("equal sub arrays are",s1, s2)
 }
